09c061bf7518460ada39cce837f09c91ee578136
diego <diego.martignoni@abenergie.it>
2023-03-01 17:19:58 +0100

    adding lazy loading image

--- src/components/Posts/PostsList.component.jsx
+++ src/components/Posts/PostsList.component.jsx
@@ -1,3 +1,4 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Link } from "react-router-dom";
 
 export default function PostsList(props){
@@ -7,7 +8,7 @@ export default function PostsList(props){
             <Link key={post.id} to={post.id}>
                 <div className="flex flex-col items-center p-10 gap-4 border-slate-900 border-t-2">
                     <h1 className="font-sans text-sm font-extrabold w-full">_ LEGGI</h1>
-                    {typeof post.cover !== "undefined" ? (<img className="w-60 h-60 object-cover border-2 border-slate-900" alt={post.id} src={post.cover}></img>) : null}
+                    {typeof post.cover !== "undefined" ? (<LazyLoadImage className="w-60 h-60 object-cover border-2 border-slate-900" alt={post.id} src={`/images/${post.cover}.jpg`} placeholderSrc={`/images/placeholder_${post.cover}.jpg`} />) : null}
                     <h2 className="font-serif text-3xl hover:underline hover:underline-offset-1">{post.title}</h2>
                     {typeof post.cover === "undefined" ? (<p className="text-sm">{`${post.description.slice(0,128)}...`}</p>) : null}
                 </div>
--- src/redux/posts/posts.reducer.js
+++ src/redux/posts/posts.reducer.js
@@ -5,7 +5,7 @@ const initialState = [
         id: "sample1",
         title: "L'arte oggi, ecco come gli NFT stanno cambiando l'arte",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin congue euismod est, ut mollis tellus consequat sit amet. Aliquam lacinia finibus convallis. Donec pretium eros erat, quis iaculis sapien pulvinar viverra. Fusce in nisl vitae turpis ultricies consequat. Suspendisse et dictum ipsum, sodales sollicitudin nisl. Donec vitae lectus ut tortor finibus varius vitae eget sem. Integer enim velit, facilisis et nibh eget, sagittis ornare mi.",
-        cover: "/images/sample1.jpg"
+        cover: "sample1"
     },
     {
         id: "sample2",
@@ -16,7 +16,7 @@ const initialState = [
         id: "sample3",
         title: "Blender in pensione, arriva un nuovo strumento sul mercato per la 3D Art",
         description: "Nam tincidunt lectus eu mi sagittis pulvinar. Cras lorem turpis, sagittis vitae odio et, vehicula vestibulum justo. Nulla rutrum risus a lacus aliquet, vitae ultrices urna bibendum. Pellentesque feugiat velit libero, at porta nunc sagittis et. Fusce lacinia et ipsum vel feugiat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc dignissim tortor at nisl laoreet, eu fringilla nisl luctus. Maecenas dignissim eu diam sed placerat.",
-        cover: "/images/sample2.jpg"
+        cover: "sample2"
     }
 ]
 
