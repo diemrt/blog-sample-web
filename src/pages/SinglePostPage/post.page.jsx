07b4700800b38ba3952c8864daf8348741c4e2a9
diego <diego.martignoni@abenergie.it>
2023-03-01 16:44:47 +0100

    fixing single post aestetic

--- src/pages/SinglePostPage/post.page.jsx
+++ src/pages/SinglePostPage/post.page.jsx
@@ -1,27 +1,20 @@
 import { useSelector } from "react-redux";
-import { Outlet, useParams } from "react-router-dom"
+import { Navigate, Outlet, useParams } from "react-router-dom"
 
 export default function PostPage(){
     const {postId} = useParams();
     const post = useSelector(state => state.posts.find(post => post.id === postId))
 
-    if(!post){
-    return (
-        <section>
-        <h2>Post not found!</h2>
-        </section>
-    )
-    }
+    if(!post)
+        return <Navigate to="/posts" />;
 
     return (
-        <div className='flex flex-col'>
-            <section>
-                <article className="flex flex-col">
-                    <h2>{post.title}</h2>
-                    <p className="post-content">{post.description}</p>
-                </article>
-            </section>
-        <Outlet />
-        </div>
+        <section className='flex flex-col p-10 gap-4'>
+            <article className="flex flex-col items-center gap-10">
+                <h2 className="font-serif text-3xl">{post.title}</h2>
+                {typeof post.cover !== "undefined" ? (<img className="h-60 object-cover border-2 border-slate-900" alt={post.id} src={post.cover}></img>) : null}
+                <p className="post-content">{post.description}</p>
+            </article>
+        </section>
     )
 }
