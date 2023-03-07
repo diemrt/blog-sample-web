b038342d490d5605808eff644f97cfc01dd0c621
diego <diego.martignoni@abenergie.it>
2023-03-07 10:27:27 +0100

    refatoring title

--- src/components/Posts/form/AddPost/AddPost.component.jsx
+++ src/components/Posts/form/AddPost/AddPost.component.jsx
@@ -1,5 +1,4 @@
-import autosize from "autosize";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { postAdded } from "../../../../redux/posts/posts.reducer";
 import PostDescription from "../PostDescription/PostDescription.components";
@@ -10,7 +9,6 @@ export default function AddPost() {
     const [description, setDescription] = useState('')
     const onTitleChange = (e) => setTitle(e.target.value);
     const onDescriptionChange = (e) => setDescription(e.target.value);
-    const titleFocus = useRef(null);
 
     const dispatch = useDispatch();
     const onSavePostClicked = () => {
@@ -21,15 +19,11 @@ export default function AddPost() {
         }
     }
 
-    useEffect(() => {
-        autosize(document.querySelectorAll('textarea'));
-    });
-
     return (
         <section className="flex flex-col p-10 gap-4 border-slate-900">
             <h1 className="font-sans text-sm font-extrabold">_ SCRIVI</h1>
             <form className="flex flex-col items-center gap-4">
-                <PostTitle {...{title, titleFocus, onTitleChange, isLocked: false}} />
+                <PostTitle {...{title, onTitleChange, isLocked: false}} />
                 <PostDescription {...{description, onDescriptionChange, isLocked: false}} />
                 <button type="button" className="flex flex-row items-center gap-2 p-2 pr-6 pl-6 border-2 border-slate-900 hover:bg-slate-900 hover:text-white active:translate-y-1 font-sans font-bold text-sm w-fit" onClick={onSavePostClicked}>
                     SALVA
--- src/components/Posts/form/PostTitle/PostTitle.component.jsx
+++ src/components/Posts/form/PostTitle/PostTitle.component.jsx
@@ -1,6 +1,19 @@
+import autosize from "autosize";
+import { useEffect, useRef } from "react";
+
 export default function PostTitle(props){
-    const {titleFocus, title, onTitleChange, isLocked} = props;
+    const {title, onTitleChange, isLocked} = props;
+    const titleRef = useRef(null);
+
+    useEffect(() => {
+        autosize(document.querySelectorAll('textarea'));
+        if (!isLocked) {
+            titleRef.current.focus();
+        }
+    }, [isLocked]);
+
+
     return (
-        <textarea ref={titleFocus} className="font-serif text-3xl bg-transparent text-left h-fit w-full" type="text" id="postTitle" name="postTitle" value={title} onChange={onTitleChange} placeholder="Titolo" disabled={isLocked}/>
+        <textarea ref={titleRef} className="font-serif text-3xl bg-transparent text-left h-fit w-full" type="text" id="postTitle" name="postTitle" value={title} onChange={onTitleChange} placeholder="Titolo" disabled={isLocked}/>
     );
 }
