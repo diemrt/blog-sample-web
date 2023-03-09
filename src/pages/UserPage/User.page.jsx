c8acb9e6d8c4b37f2ce3380351a914455ec222d6
diego <diego.martignoni@abenergie.it>
2023-03-09 10:38:18 +0100

    adding user page info

--- src/pages/UserPage/User.page.jsx
+++ src/pages/UserPage/User.page.jsx
@@ -1,9 +1,17 @@
-export default function UserPage(){
+import { useSelector } from "react-redux";
 
+export default function UserPage(){
+    const loggedUser = useSelector(state => state.firebase);
+    const user = useSelector(state => state.users.find(u => u.id === loggedUser.userId));
     return (
         <div className="p-10 flex flex-row justify-between">
-            <div className="flex flex-row gap-3">
-                Hi
+            <div className="flex flex-col gap-5">
+                <span className="rounded-full h-20 w-20 flex justify-center items-center text-4xl font-extrabold bg-slate-900 text-white">{user.name.charAt(0).toUpperCase()}</span>
+                <section className="flex flex-col gap-1">
+                    <span className="text-xl">{user.name}</span>
+                    <span className="text-xs">{user.email}</span>
+                </section>
+                <small className="text-sm">{user.bio}</small>
             </div>
         </div>
     )
