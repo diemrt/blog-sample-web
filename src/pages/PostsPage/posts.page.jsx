f952f7349ede51597524627dc40667641affb5ac
diego <diego.martignoni@abenergie.it>
2023-03-14 11:46:33 +0100

    refactoring posts page

--- src/pages/PostsPage/posts.page.jsx
+++ src/pages/PostsPage/posts.page.jsx
@@ -1,10 +1,7 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import Editor from "../../components/Posts/Editor/Editor.component";
 import AddPost from "../../components/Posts/form/AddPost/AddPost.component";
 import ListWrapper from "../../components/Posts/ListWrapper/ListWrapper.component";
-import { fetchPosts, getAllPosts } from "../../redux/posts/posts.reducer";
 
 export default function PostsPage(){
     const postEditor = Editor(AddPost, {...{lockState: false}});
